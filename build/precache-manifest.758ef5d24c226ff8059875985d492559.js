d43fe1093a6d36c08bb22356a1aefe42b58631b6
abdulsalam alkhateeb <abdulsalam@matched.se>
2021-01-31 14:20:20 +0300

    Initial commit

--- build/precache-manifest.758ef5d24c226ff8059875985d492559.js
+++ build/precache-manifest.758ef5d24c226ff8059875985d492559.js
@@ -1,26 +1,26 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
     "revision": "896956d85f5b5fd50d05c2d239f387be",
-    "url": "/index.html"
+    "url": "index.html"
   },
   {
     "revision": "d3893fa1f38342ec229f",
-    "url": "/static/css/main.fa1d4784.chunk.css"
+    "url": "static/css/main.fa1d4784.chunk.css"
   },
   {
     "revision": "b8ebce353c27c7e4bcd4",
-    "url": "/static/js/2.b658319d.chunk.js"
+    "url": "static/js/2.b658319d.chunk.js"
   },
   {
     "revision": "402ef71888331977f0b109e60b155b64",
-    "url": "/static/js/2.b658319d.chunk.js.LICENSE.txt"
+    "url": "static/js/2.b658319d.chunk.js.LICENSE.txt"
   },
   {
     "revision": "d3893fa1f38342ec229f",
-    "url": "/static/js/main.3ba0bd7b.chunk.js"
+    "url": "static/js/main.3ba0bd7b.chunk.js"
   },
   {
     "revision": "4f2b1704eab49d50853b",
-    "url": "/static/js/runtime-main.19ccc7f6.js"
+    "url": "static/js/runtime-main.19ccc7f6.js"
   }
 ]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.758ef5d24c226ff8059875985d492559.js"
+  "precache-manifest.758ef5d24c226ff8059875985d492559.js"
 );
 
 self.addEventListener('message', (event) => {
